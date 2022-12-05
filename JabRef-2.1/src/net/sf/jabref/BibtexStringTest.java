6cf8b128d989f21c6c6024f197994682101e232f
Prakash0030 <spathuru@uccs.edu>
2022-12-04 21:15:26 -0700

    latest push

--- JabRef-2.1/src/net/sf/jabref/BibtexStringTest.java
+++ JabRef-2.1/src/net/sf/jabref/BibtexStringTest.java
@@ -1,14 +1,12 @@
 package net.sf.jabref;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Test;
 
-import net.sf.jabref.BibtexString;
-
-class BibtexStringTest {
+public class BibtexStringTest {
     @Test
-    void testConstructor() {
+    public void testConstructor() {
         BibtexString actualBibtexString = new BibtexString("42", "Name", "Not all who wander are lost");
         actualBibtexString.setContent("Not all who wander are lost");
         actualBibtexString.setId("42");
@@ -17,12 +15,12 @@ class BibtexStringTest {
         assertEquals("Name", actualBibtexString.getName());
     }
     @Test
-    void testGetContent() {
+    public void testGetContent() {
         assertEquals("Not all who wander are lost",(new BibtexString("42", "Name", "Not all who wander are lost")).getContent());
         assertEquals("", (new BibtexString("42", "Name", null)).getContent());
     }
     @Test
-    void testClone() {
+    public void testClone() {
         assertEquals("Not all who wander are lost",((BibtexString) (new BibtexString("42", "Name", "Not all who wander are lost")).clone()).getContent());
         assertEquals("Name",((BibtexString) (new BibtexString("42", "Name", "Not all who wander are lost")).clone()).getName());
         assertEquals("42",((BibtexString) (new BibtexString("42", "Name", "Not all who wander are lost")).clone()).getId());
